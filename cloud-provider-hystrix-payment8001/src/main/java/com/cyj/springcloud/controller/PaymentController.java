0a052cf248adfb5e3690b6a67814c374d7f3907b
user <643379347@qq.com>
2020-09-03 17:28:14 +0800

    新增：服务熔断功能

--- cloud-provider-hystrix-payment8001/src/main/java/com/cyj/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/cyj/springcloud/controller/PaymentController.java
@@ -16,16 +16,22 @@ public class PaymentController {
     private PaymentService paymentService;
     @Value("${server.port}")
     private String serverPort;
-    @GetMapping("payment/hystrix/ok/{id}")
+    @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id){
         String result=paymentService.paymentInfo_OK(id);
         log.info("****result "+result);
         return result;
     }
-    @GetMapping("payment/hystrix/timeout/{id}")
+    @GetMapping("/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result=paymentService.paymentInfo_TimeOut(id);
         log.info("****result "+result);
+        return result;
+    }
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result=paymentService.paymentCircuitBreaker(id);
+        log.info("****reult: "+result);
         return result;
     }
 }
